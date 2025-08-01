023aa4acf56b44b911598ae130247028c708d69e
ElRobertRocha91 <robertoestebanrocha91@gmail.com>
2025-07-31 21:07:28 -0300

    feat(model): Add Role in model

---
+++ demo/src/main/java/com/TechLab/spring/model/Role.java
@@ -0,0 +1,21 @@
+package com.TechLab.spring.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String nombre;
+}
