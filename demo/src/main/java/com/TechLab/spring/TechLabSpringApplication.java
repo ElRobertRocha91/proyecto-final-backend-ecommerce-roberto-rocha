71a6811dbfe8cc654bfc3bc77b1db2008966cbcd
ElRobertRocha91 <robertoestebanrocha91@gmail.com>
2025-08-08 10:26:35 -0300

    feat(main): Add access configuration with environment variables - dotenv

--- demo/src/main/java/com/TechLab/spring/TechLabSpringApplication.java
+++ demo/src/main/java/com/TechLab/spring/TechLabSpringApplication.java
@@ -1,13 +1,23 @@
 package com.TechLab.spring;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class TechLabSpringApplication {
-
 	public static void main(String[] args) {
+		// Agregando variables de entorno
+		Dotenv dotenv = Dotenv.load();
+		String dbPassword = dotenv.get("PASSWORD_DATABASE");
+		// Validación
+		if(dbPassword != null) {
+			System.setProperty("PASSWORD_DATABASE", dbPassword);
+		} else {
+			System.out.println("La clave de acceso de .env no esta");
+		}
+
 		SpringApplication.run(TechLabSpringApplication.class, args);
 	}
 
